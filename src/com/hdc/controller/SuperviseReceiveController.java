85c0ac437d0757f4ff1ef18f45109db1f1d93814
zhao <zhao@zhao-PC.lan>
2016-02-25 21:15:46 +0800

    1

---
+++ src/com/hdc/controller/SuperviseReceiveController.java
@@ -0,0 +1,26 @@
+package com.hdc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 督察接收
+ * @author zhao
+ *
+ */
+@Controller
+@RequestMapping("/superviserReveive")
+public class SuperviseReceiveController {
+
+	/**
+	 * 跳转到督察接收
+	 * @return
+	 */
+	@RequestMapping(value = "/toSuperviseReceive")
+	public ModelAndView toSuperviseReceive() {
+		ModelAndView mv = new ModelAndView("/superviseReceive/list_receive");
+		return mv;
+	}
+	
+}
