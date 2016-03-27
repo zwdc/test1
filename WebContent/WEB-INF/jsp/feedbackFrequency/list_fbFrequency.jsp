5d95268a2596182817b7e3d40f789103233ab8a7
liuyadi <liuyadi@geek-zoo.com>
2016-03-28 00:22:24 +0800

    反馈频度页面

---
+++ WebContent/WEB-INF/jsp/feedbackFrequency/list_fbFrequency.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%@ include file="/WEB-INF/taglibs/taglibs.jsp"%>
+<%@ taglib prefix="shiro" uri="http://shiro.apache.org/tags" %>
+
+<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+	<title>反馈频度管理</title>
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="expires" content="0">    
+	<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
+	<meta http-equiv="description" content="This is my page">
+	<script type="text/javascript" src="${ctx}/kindeditor/kindeditor-min.js"></script>
+  	<script type="text/javascript" src="${ctx}/js/kindeditor.js"></script>
+	<script type="text/javascript" src="${ctx}/js/app/feedbackFrequency.js?_=${sysInitTime}"></script>
+  </head>
+  <body>
+	<div id="toolbar" style="padding:2px 0">
+		<table>
+			<tr>
+				<td style="padding-left:2px">
+					<shiro:hasRole name="admin">
+						<a href="javascript:void(0);" class="easyui-linkbutton" data-options="iconCls:'icon-add',plain:true" onclick="add();">添加</a>
+						<a href="javascript:void(0);" class="easyui-linkbutton" data-options="iconCls:'icon-edit',plain:true" onclick="edit();">编辑</a>
+						<a href="javascript:void(0);" class="easyui-linkbutton" data-options="iconCls:'icon-remove',plain:true" onclick="del();">删除</a>
+						<a href="javascript:void(0);" class="easyui-linkbutton" data-options="iconCls:'icon-search',plain:true" onclick="details();">详情</a>
+					</shiro:hasRole>
+				</td>
+				<td style="padding-left:5px">
+					<input id="searchbox" type="text"/>
+				</td>
+			</tr>
+		</table>
+	</div>
+	
+	<div id="searchMenu" style="display: none;">
+		<div data-options="name:'name'">来源名称</div>
+		<div data-options="name:'createDate'">创建日期</div>
+	</div>
+	
+	<table id="taskSource_datagrid" title="反馈频度管理"></table>
+  </body>
+</html>
---
+++ WebContent/js/app/feedbackFrequency.js
@@ -0,0 +1,203 @@
+/**
+ * 反馈频度
+ */
+
+var feedback_datagrid;
+var feedback_form;
+var feedback_dialog;
+
+$(function() {
+	//数据列表
+	feedback_datagrid = $('#feedback_datagrid').datagrid({
+        url: ctx+"/feedbackFrequency/getList",
+        width : 'auto',
+		height : fixHeight(1),
+		pagination:true,
+		rownumbers:true,
+		border:false,
+		singleSelect:true,
+		striped:true,
+        columns : [ 
+            [ 
+              {field: 'name', title: '频度名称', width: fixWidth(0.3), align: 'left', halign: 'center', sortable: true},
+              {field: '', title: '频度类型', width: fixWidth(0.4), align: 'left', halign: 'center'},
+              {field: 'createDate', title: '创建日期', width: fixWidth(0.1), align: 'center', sortable: true}
+    	    ] 
+        ],
+        toolbar: "#toolbar"
+    });
+    
+	$("#searchbox").searchbox({ 
+		menu:"#searchMenu", 
+		prompt :'模糊查询',
+	    searcher:function(value,name){   
+	    	var str="{\"searchName\":\""+name+"\",\"searchValue\":\""+value+"\"}";
+            var obj = eval('('+str+')');
+            feedback_datagrid.datagrid('reload',obj); 
+	    }
+	});
+});
+
+//修正宽高
+function fixHeight(percent) {   
+	return parseInt($(this).height() * percent);
+}
+
+function fixWidth(percent) {   
+	return parseInt(($(this).width() - 50) * percent);
+}
+
+//初始化表单
+function formInit(row) {
+	feedback_form = $('#taskSourceForm').form({
+        url: ctx+"/feedbackFrequency/saveOrUpdate",
+        onSubmit: function () {
+	        $.messager.progress({
+	            title: '提示信息！',
+	            text: '数据处理中，请稍后....'
+	        });
+	        var isValid = $(this).form('validate');
+	        if (!isValid) {
+	            $.messager.progress('close');
+	        }
+	        return isValid;
+	    },
+	    success: function (data) {
+	        $.messager.progress('close');
+	        var json = $.parseJSON(data);
+	        if (json.status) {
+	        	feedback_dialog.dialog('destroy');
+	        	feedback_datagrid.datagrid('load');
+	        	
+	        } 
+	        $.messager.show({
+				title : json.title,
+				msg : json.message,
+				timeout : 1000 * 2
+			});
+	    }
+    });
+}
+
+//添加
+function add(row) {
+	var _url = ctx+"/feedbackFrequency/toMain";
+	if(row) {
+		_url = ctx+"/feedbackFrequency/toMain?id="+row.id;
+	}
+	feedback_dialog = $('<div/>').dialog({
+    	title : "频度信息",
+		top: 20,
+		width : fixWidth(0.8),
+		height : 'auto',
+        modal: true,
+        minimizable: true,
+        maximizable: true,
+        href: _url,
+        onLoad: function () {
+            formInit(row);
+        },
+        buttons: [
+            {
+                text: '保存',
+                iconCls: 'icon-save',
+                handler: function () {
+                	feedback_form.submit();
+                }
+            },
+            {
+                text: '重置',
+                iconCls: 'icon-reload',
+                handler: function () {
+                	feedback_form.form('clear');
+                }
+            },
+            {
+                text: '关闭',
+                iconCls: 'icon-cancel',
+                handler: function () {
+                	feedback_dialog.dialog('destroy');
+                }
+            }
+        ],
+        onClose: function () {
+        	feedback_dialog.dialog('destroy');
+        }
+    });
+}
+
+//编辑
+function edit() {
+    var row = feedback_datagrid.datagrid('getSelected');
+    if (row) {
+    	add(row);
+    } else {
+        $.messager.alert("提示", "您未选择任何操作对象，请选择一行数据！");
+    }
+}
+
+//删除
+function del() {
+    var row = feedback_datagrid.datagrid('getSelected');
+    if (row) {
+        $.messager.confirm('确认提示！', '您确定要删除选中的数据?', function (result) {
+            if (result) {
+                $.ajax({
+            		async: false,
+            		cache: false,
+                    url: ctx + '/feedbackFrequency/delete/'+row.id,
+                    type: 'post',
+                    dataType: 'json',
+                    data: {},
+                    success: function (data) {
+                        if (data.status) {
+                        	feedback_datagrid.datagrid('load');
+                        }
+                        $.messager.show({
+        					title : data.title,
+        					msg : data.message,
+        					timeout : 1000 * 2
+        				});
+                    }
+                });
+            }
+        });
+    } else {
+    	$.messager.alert("提示", "您未选择任何操作对象，请选择一行数据！");
+    }
+}
+
+function details(){
+    var row = feedback_datagrid.datagrid('getSelected');
+    if (row) {
+    	showDetails(row);
+    } else {
+        $.messager.alert("提示", "您未选择任何操作对象，请选择一行数据！");
+    }
+}
+
+function showDetails(row) {
+	source_dialog = $('<div/>').dialog({
+    	title : "频度详情",
+		top: 20,
+		width : fixWidth(0.8),
+		height : 'auto',
+        modal: true,
+        minimizable: true,
+        maximizable: true,
+        href: ctx+"/feedbackFrequency/details/"+row.id,
+        buttons: [
+            {
+                text: '关闭',
+                iconCls: 'icon-cancel',
+                handler: function () {
+                	source_dialog.dialog('destroy');
+                }
+            }
+        ],
+        onClose: function () {
+        	source_dialog.dialog('destroy');
+        }
+    });
+}
+
