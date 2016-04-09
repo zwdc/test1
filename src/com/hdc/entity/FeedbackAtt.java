429ad36d2e764c5207e7522c6be7849086ea77d4
zhao <zhao@192.168.1.108>
2016-04-09 14:39:49 +0800

    修改

--- src/com/hdc/entity/FeedbackAtt.java
+++ src/com/hdc/entity/FeedbackAtt.java
@@ -4,14 +4,16 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+/**
+ * 反馈附件
+ * @author Administrator
+ *
+ */
 @Entity
 @Table(name="feedback_att")
 public class FeedbackAtt implements Serializable{
@@ -25,26 +27,24 @@ public class FeedbackAtt implements Serializable{
 	@Column(name = "id", length = 10, nullable = false, unique = true)
 	private Integer id;
 	
-	@ManyToOne(fetch = FetchType.LAZY)
+/*	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name="feedbackRecord_id")
-	private FeedbackRecord fdRecord;
+	private FeedbackRecord fdRecord;*/
+	
 	@Column(name = "name", length = 50)
 	private String name;
+	
 	@Column(name = "url", length = 200)
 	private String url;
+	
 	@Column(name = "type", length = 10)
 	private String type;
+	
 	public Integer getId() {
 		return id;
 	}
 	public void setId(Integer id) {
 		this.id = id;
-	}
-	public FeedbackRecord getFdRecord() {
-		return fdRecord;
-	}
-	public void setFdRecord(FeedbackRecord fdRecord) {
-		this.fdRecord = fdRecord;
 	}
 	public String getName() {
 		return name;
