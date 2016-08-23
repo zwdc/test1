70af1201a837af116c3f73c6219fd8f60644d866
zhaoml529 <zhaoml529@163.com>
2016-08-23 14:40:51 +0800

    清理垃圾

--- src/com/hdc/controller/ProjectScoreController.java
+++ src/com/hdc/controller/ProjectScoreController.java
@@ -1,38 +1,17 @@
 package com.hdc.controller;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.fileupload.FileUploadBase.FileSizeLimitExceededException;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.hdc.entity.Datagrid;
-import com.hdc.entity.Message;
 import com.hdc.entity.Page;
 import com.hdc.entity.Parameter;
 import com.hdc.entity.ProjectScore;
-import com.hdc.entity.TaskSource;
 import com.hdc.service.IProjectScoreService;
-import com.hdc.service.ITaskSourceService;
-import com.hdc.util.BeanUtilsExt;
-import com.hdc.util.Constants;
-import com.hdc.util.FileDownloadUtils;
-import com.hdc.util.upload.FileUploadUtils;
-import com.hdc.util.upload.exception.InvalidExtensionException;
 
 /**
  * 项目评分表
